e37bcc695ad1062a2f40e6e0c0ac1ba055526be8
abeizer <abby@regression.gg>
2022-11-09 12:49:03 -0500

    Add content to README

--- README.md
+++ README.md
@@ -1,2 +1,49 @@
 # AdvancedBotTemplate
 An advanced example of how to customize movements, craft items, and place blocks using the rg-bot package
+
+# Regression Games Bot Template
+
+This template demonstrates how to use advanced features of the [rg-bot package](https://www.npmjs.com/package/rg-bot) to create
+a bot for the Regression Games: Ultimate Collector challenge.
+
+## Minimum Requirements for Regression Games
+
+Every bot must have an `index.js` file with the following code:
+
+```javascript
+function configureBot(bot) {
+  // Bot logic here
+}
+
+exports.configureBot = configureBot
+```
+
+This defines a `configureBot` function and exposes that function to Regression Games.
+Regression Games uses it as an entrypoint to your bot script, and passes a bot for you to interact with.
+
+Here is an example of the `configureBot` function with some basic logic that will make your bot parrot back
+anything it sees in chat from other players.
+
+```javascript
+function configureBot(bot) {
+
+  // Every time a player says something in the game, 
+  // do something with that player's username and their message
+  bot.on('chat', (username, message) => { 
+  
+      // If the username of the speaker is equal to the username of this bot, 
+      // don't do anything else. This is because we don't want the bot to repeat 
+      // something that it says itself, or else it will spam the chat and be 
+      // kicked from the game!
+      if (username === bot.mineflayer().username) return
+      
+      // make the bot chat with the same message the other player sent
+      bot.chat("This is what I heard: " + message)
+  })
+  
+}
+
+exports.configureBot = configureBot
+```
+
+_Not sure what this is? Visit https://regression.gg for some programming fun!_
--- index.js
+++ index.js
@@ -1,12 +1,14 @@
-const RGBot = require('rg-bot').RGBot;
+/**
+ * This strategy is an advanced example of how to customize movements, place blocks, and craft items with the rg-bot package.
+ * The Bot will chop wood until it has 100 points-worth of items in its inventory.
+ * (Note: Logs and apples are each worth 1 point)
+ *
+ * @param {RGBot} bot
+ */
 
-// This strategy is an advanced example of how to customize movements, place blocks, and craft items with the rg-bot package.
-// The Bot will chop wood until it has 100 points-worth of items in its inventory.
-// (Note: Logs and apples are each worth 1 point)
 function configureBot(bot) {
 
-    const rg = new RGBot(bot);
-    rg.setDebug(true);
+    bot.setDebug(true);
 
     // This function will make the Bot chop + pick up a Spruce Log.
     async function gatherLog() {
@@ -15,15 +17,15 @@ function configureBot(bot) {
         // There are so many trees around the spawn area that it can
         // simply try to chop a different one
         let skipCurrentLog = false;
-        const logsBefore = rg.getInventoryItemQuantity('spruce_log');
+        const logsBefore = bot.getInventoryItemQuantity('spruce_log');
 
         // Ensure that if the Bot fails to gather the dropped log,
         // it will try collecting another log until its inventory reflects one has been picked up
-        while (rg.getInventoryItemQuantity('spruce_log') <= logsBefore) {
-            const foundLog = await rg.findBlock('spruce_log', {skipClosest: skipCurrentLog});
+        while (bot.getInventoryItemQuantity('spruce_log') <= logsBefore) {
+            const foundLog = await bot.findBlock('spruce_log', {skipClosest: skipCurrentLog});
             if (foundLog) {
                 // If the Bot located a spruce log, then go chop it
-                const success = await rg.findAndDigBlock('spruce_log', {skipClosest: skipCurrentLog});
+                const success = await bot.findAndDigBlock('spruce_log', {skipClosest: skipCurrentLog});
                 if (!success) {
                     // If anything prevents the Bot from breaking the block,
                     // then find the next-closest log and try chopping that instead.
@@ -37,7 +39,7 @@ function configureBot(bot) {
                 // This loop makes sure it completes the 'wander' movement.
                 let didWander = false;
                 while (!didWander) {
-                    didWander = await rg.wander();
+                    didWander = await bot.wander();
                 }
             }
         }
@@ -45,49 +47,49 @@ function configureBot(bot) {
 
     // The bot will announce whenever it collects a log or an apple
     bot.on('playerCollect', async (collector, collected) => {
-        const itemName = rg.getEntityName(collected).toLowerCase();
-        if (collector.username === bot.username && (itemName.includes('log') || itemName === 'apple')) {
-            rg.chat(`I collected a(n) ${itemName}`);
+        const itemName = bot.getEntityName(collected).toLowerCase();
+        if (collector.username === bot.mineflayer().username && (itemName.includes('log') || itemName === 'apple')) {
+            bot.chat(`I collected a(n) ${itemName}`);
         }
     });
 
     async function craftTable() {
         // If the Bot doesn't already have a craftingTable, then 4 planks are needed to craft one.
         // The Bot can get planks from 1 log if needed.
-        if (!rg.inventoryContainsItem('crafting_table')) {
-            if (!rg.inventoryContainsItem('spruce_planks', { quantity: 4 })) {
-                if (!rg.inventoryContainsItem('spruce_log')) {
+        if (!bot.inventoryContainsItem('crafting_table')) {
+            if (!bot.inventoryContainsItem('spruce_planks', { quantity: 4 })) {
+                if (!bot.inventoryContainsItem('spruce_log')) {
                     await gatherLog();
                 }
-                await rg.craftItem('spruce_planks');
+                await bot.craftItem('spruce_planks');
             }
-            await rg.craftItem('crafting_table');
+            await bot.craftItem('crafting_table');
         }
     }
 
     async function craftSticks() {
         // If the Bot doesn't have 4 sticks, then 2 planks are needed to craft them.
         // The Bot can get planks from 1 log if needed.
-        if (!rg.inventoryContainsItem('stick', { quantity: 4 })) {
-            if (!rg.inventoryContainsItem('spuce_planks', { quantity: 2 })) {
-                if (!rg.inventoryContainsItem('spruce_log')) {
+        if (!bot.inventoryContainsItem('stick', { quantity: 4 })) {
+            if (!bot.inventoryContainsItem('spuce_planks', { quantity: 2 })) {
+                if (!bot.inventoryContainsItem('spruce_log')) {
                     await gatherLog();
                 }
-                await rg.craftItem('spruce_planks');
+                await bot.craftItem('spruce_planks');
             }
-            await rg.craftItem('stick');
+            await bot.craftItem('stick');
         }
     }
 
     async function craftPlanks() {
         // If the Bot doesn't have 6 spruce planks, then 2 logs are needed to craft them.
-        if (!rg.inventoryContainsItem('spruce_planks', { quantity: 6 })) {
-            const logsCarried = rg.getInventoryItemQuantity('spruce_log');
-            const logsNeeded = (rg.getInventoryItemQuantity('spruce_planks')) >= 2 ? 1 : 2;
+        if (!bot.inventoryContainsItem('spruce_planks', { quantity: 6 })) {
+            const logsCarried = bot.getInventoryItemQuantity('spruce_log');
+            const logsNeeded = (bot.getInventoryItemQuantity('spruce_planks')) >= 2 ? 1 : 2;
             for (let i = logsCarried; i < logsNeeded; i++) {
                 await gatherLog();
             }
-            await rg.craftItem('spruce_planks', { quantity: logsNeeded });
+            await bot.craftItem('spruce_planks', { quantity: logsNeeded });
         }
     }
 
@@ -102,26 +104,26 @@ function configureBot(bot) {
 
         // Finally, craft the axes
         // Locate a spot to place the craftingTable, place it, then stand next to it
-        const ground = rg.findBlock('grass', {onlyFindTopBlocks: true, maxDistance: 20}) || rg.findBlock('dirt', { onlyFindTopBlocks: true, maxDistance: 20});
-        await rg.placeBlock('crafting_table', ground);
-        const placedTable = await rg.findBlock('crafting_table');
-        await rg.approachBlock(placedTable);
+        const ground = bot.findBlock('grass', {onlyFindTopBlocks: true, maxDistance: 20}) || bot.findBlock('dirt', { onlyFindTopBlocks: true, maxDistance: 20});
+        await bot.placeBlock('crafting_table', ground);
+        const placedTable = await bot.findBlock('crafting_table');
+        await bot.approachBlock(placedTable);
 
         // Craft 2 axes and equip one of them, then gather the crafting table
-        await rg.craftItem('wooden_axe', { quantity: 2, craftingTable: placedTable });
-        await rg.holdItem('wooden_axe');
-        await rg.findAndDigBlock('crafting_table');
+        await bot.craftItem('wooden_axe', { quantity: 2, craftingTable: placedTable });
+        await bot.holdItem('wooden_axe');
+        await bot.findAndDigBlock('crafting_table');
     }
 
     // When the Bot spawns, begin the main gathering loop.
     // Before collecting a log, have the Bot craft axes if it has none.
     bot.on('spawn', async () => {
-        rg.chat('Hello, I have arrived!');
+        bot.chat('Hello, I have arrived!');
 
-        let logsCollected = rg.getInventoryItemQuantity('spruce_log');
-        let applesCollected = rg.getInventoryItemQuantity('apple');
+        let logsCollected = bot.getInventoryItemQuantity('spruce_log');
+        let applesCollected = bot.getInventoryItemQuantity('apple');
         while (logsCollected + applesCollected < 100) {
-            if (!rg.inventoryContainsItem('_axe', {partialMatch: true})) {
+            if (!bot.inventoryContainsItem('_axe', {partialMatch: true})) {
                 // craft axes if inventory doesn't have any
                 await craftAxes();
             }
@@ -129,7 +131,7 @@ function configureBot(bot) {
         }
 
         // Once the Bot has 100 points, announce it in the chat
-        rg.chat(`I reached my goal! I have ${logsCollected} logs and ${applesCollected} apples`);
+        bot.chat(`I reached my goal! I have ${logsCollected} logs and ${applesCollected} apples`);
     });
 
 }
